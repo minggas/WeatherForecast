63e4d174e43eadb61a19758e0fa149b8ee1d272f
minggas <fasfora@gmail.com>
2018-04-02 06:22:00 -0300

    Some JS changes, add babel, jshint and eslint

--- js/main.js
+++ js/main.js
@@ -1,53 +1,58 @@
+/* jshint browser: true */
 /* geolocation is available */
 /* Put the DOM Elements in variables */
+(function () {
+    'use strict';
+    const tempMap = {
+        main: 0,
+        min: 0,
+        max: 0
+    };
+    const city = document.getElementById('city');
+    const temperature = document.getElementById('temperature');
+    const min = document.getElementById('min');
+    const max = document.getElementById('max');
+    const description = document.getElementById('description');
+    const icon = document.getElementById('icon');
+    //const pressure = document.getElementById('pressure');
+    //const humidity = document.getElementById('humidity');
+    //const wSpeed = document.getElementById('test4');
+    temperature.addEventListener('click', toggleTemp);
+    geoFindMe();
+    //Get Latitude and Longitude
+    function geoFindMe() {
+        if (!navigator.geolocation) {
+            city.innerHTML = "<p>Geolocation is not supported by your browser</p>";
 
-const tempMap = {
-  main: 0,
-  min: 0,
-  max: 0
-}
-const city = document.getElementById('city');
-const temperature = document.getElementById('temperature');
-const min = document.getElementById('min');
-const max = document.getElementById('max');
-const description = document.getElementById('description');
-const icon = document.getElementById('icon');
-//const pressure = document.getElementById('pressure');
-//const humidity = document.getElementById('humidity');
-//const wSpeed = document.getElementById('test4');
-temperature.addEventListener('click', toggleTemp);
+            return;
+        }
+        function success(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const end = `https://fcc-weather-api.glitch.me/api/current?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}`;
+            getData(end);
+        }
 
-//Get Latitude and Longitude
-if ("geolocation" in navigator) {
-    /* Get location */
-    const geo = navigator.geolocation.getCurrentPosition(function (position) {
-        let lat = parseFloat(position.coords.latitude);
-        let lon = parseFloat(position.coords.longitude);
-        let end = `https://fcc-weather-api.glitch.me/api/current?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}`;
-        //call FCC Weather API
-        getData(end);
-    });
-} else {
-    /* geolocation IS NOT available */
-    document.getElementById('card-content').innerHTML = '<h1>Geolocation unavailable</h1>;';
-}
-
-async function getData(end){
-    const response = await fetch(end, {cache: "no-store"});
-    if(response.status === 200 ){
-      const json = await response.json();
-      if(json.weather[0].icon){
-        renderData(json);
-      }else{
-        getData(end);
-      }
-    }else{
-      alert("Sorry for the incovenient the API return: " + response.status);
+        function error() {
+            city.innerHTML = "Unable to retrieve your location";
+        }
+        navigator.geolocation.getCurrentPosition(success, error);
     }
-  }
 
-function renderData(data){
-    //Put the temperatures variables in the tempMap
+
+    function getData(end) {
+        fetch(end, {cache: "no-store"}).then((response) => response.json()).then((myJson) => {
+            if (myJson.weather[0].icon) {
+                renderData(myJson);
+            } else {
+                getData(end);
+            }
+        }).catch(() => getData(end));
+    }
+
+
+    function renderData(data) {
+        //Put the temperatures variables in the tempMap
         tempMap.main = data.main.temp.toFixed(1);
         tempMap.min = data.main.temp_min.toFixed(1);
         tempMap.max = data.main.temp_max.toFixed(1);
@@ -63,18 +68,19 @@ function renderData(data){
         //humidity.innerHTML = data.main.humidity + '%';
         //wSpeed.innerHTML = data.wind.speed + 'm/s';
     }
-/* Function to convert celsius <-> farenheit */
-function toggleTemp(e) {
-    if (e.target.innerHTML.indexOf('C') !== -1) {
-        let tempF = (tempMap.main * 1.8 + 32).toFixed(1);
-        let tempMin = (tempMap.min * 1.8 + 32).toFixed(1);
-        let tempMax = (tempMap.max * 1.8 + 32).toFixed(1);
-        temperature.innerHTML = `${tempF}&deg; F`;
-        min.innerHTML = `MIN: ${tempMin}&deg`;
-        max.innerHTML = `MAX: ${tempMin}&deg`;
-    } else {
-        temperature.innerHTML = `${tempMap.main}&deg; C`;
-        min.innerHTML = `MIN: ${tempMap.min}&deg`;
-        max.innerHTML = `MAX: ${tempMap.max}&deg`;
+    /* Function to convert celsius <-> farenheit */
+    function toggleTemp(e) {
+        if (e.target.innerHTML.indexOf('C') !== -1) {
+            const tempF = (tempMap.main * 1.8 + 32).toFixed(1);
+            const tempMin = (tempMap.min * 1.8 + 32).toFixed(1);
+            const tempMax = (tempMap.max * 1.8 + 32).toFixed(1);
+            temperature.innerHTML = `${tempF}&deg; F`;
+            min.innerHTML = `MIN: ${tempMin}&deg`;
+            max.innerHTML = `MAX: ${tempMax}&deg`;
+        } else {
+            temperature.innerHTML = `${tempMap.main}&deg; C`;
+            min.innerHTML = `MIN: ${tempMap.min}&deg`;
+            max.innerHTML = `MAX: ${tempMap.max}&deg`;
+        }
     }
-}
+})();
